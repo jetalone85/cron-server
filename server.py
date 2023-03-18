0344fd84e9b29b8654f0694b0b8dc4360a39ab68
Marcin Pyrka <marcin.pyrka@allegro.pl>
2023-03-18 21:20:07 +0100

    Update

--- server.py
+++ server.py
@@ -6,46 +6,51 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ("/RPC2",)
 
 
-def run_grpc_server(port: int = 8000):
-    with SimpleXMLRPCServer(("localhost", port), requestHandler=RequestHandler) as server:
-        server.register_introspection_functions()
+class GRPCServer:
+    def __init__(self, port: int = 8000):
+        self.port = port
 
-        def xmlrpc(name=None):
-            def register_func(func):
-                def wrapper(*args, **kwargs):
-                    try:
-                        # Call the function and store the result in the database
-                        result = func(*args, **kwargs)
-                    except Exception as e:
-                        # Handle exceptions and log the error
-                        server.logger.error(f"Error while executing function {func.__name__}: {e}")
-                        raise
+    def start(self):
+        with SimpleXMLRPCServer(("localhost", self.port), requestHandler=RequestHandler) as server:
+            server.register_introspection_functions()
 
-                    return result
+            def xmlrpc(name=None):
+                def register_func(func):
+                    def wrapper(*args, **kwargs):
+                        try:
+                            # Call the function and store the result in the database
+                            result = func(*args, **kwargs)
+                        except Exception as e:
+                            # Handle exceptions and log the error
+                            server.logger.error(f"Error while executing function {func.__name__}: {e}")
+                            raise
 
-                server.register_function(wrapper, name)
-                return wrapper
+                        return result
 
-            return register_func
+                    server.register_function(wrapper, name)
+                    return wrapper
 
-        @xmlrpc(name="math.pow")
-        def pow(base: int, exp: int) -> int:
-            return base ** exp
+                return register_func
 
-        @xmlrpc(name="math.add")
-        def add(x: int, y: int) -> int:
-            return x + y
+            @xmlrpc(name="math.pow")
+            def pow(base: int, exp: int) -> int:
+                return base ** exp
 
-        try:
-            print("Starting XML-RPC server...")
-            server.serve_forever()
-        except KeyboardInterrupt:
-            print("Shutting down XML-RPC server...")
-            server.shutdown()
-            print("Server shutdown complete.")
+            @xmlrpc(name="math.add")
+            def add(x: int, y: int) -> int:
+                return x + y
+
+            try:
+                print("Starting XML-RPC server...")
+                server.serve_forever()
+            except KeyboardInterrupt:
+                print("Shutting down XML-RPC server...")
+                server.shutdown()
+                print("Server shutdown complete.")
 
 
-server_thread = threading.Thread(target=run_grpc_server(8000))
+server = GRPCServer(port=8000)
+server_thread = threading.Thread(target=server.start)
 server_thread.start()
 
 server_thread.join()
